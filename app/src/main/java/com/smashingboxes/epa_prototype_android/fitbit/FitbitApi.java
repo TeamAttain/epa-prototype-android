3f5b9b26d5a659db8a53a571a455a62673aacf87
Austin Lanier <austin@smashingboxes.com>
2015-12-10 17:19:25 -0500

    Adds Activity endpoint model objects

--- app/src/main/java/com/smashingboxes/epa_prototype_android/fitbit/FitbitApi.java
+++ app/src/main/java/com/smashingboxes/epa_prototype_android/fitbit/FitbitApi.java
@@ -1,6 +1,7 @@
 package com.smashingboxes.epa_prototype_android.fitbit;
 
 import com.android.volley.Response;
+import com.smashingboxes.epa_prototype_android.models.ActivityData;
 import com.smashingboxes.epa_prototype_android.models.FitbitProfile;
 
 /**
@@ -14,6 +15,7 @@ public interface FitbitApi {
      *
      * Retrieves the user by id
      *
+     * @param cancelTag
      * @param userId - The encoded ID of the user. Use "-" (dash) for current logged-in user
      * @param fitbitProfileListener
      * @param errorListener
@@ -28,11 +30,27 @@ public interface FitbitApi {
      *
      * Retrieves the current logged in user's profile
      *
+     * @param cancelTag
      * @param fitbitProfileListener
      * @param errorListener
      */
     void getCurrentUserProfile(Object cancelTag, Response.Listener<FitbitProfile> fitbitProfileListener,
                         Response.ErrorListener errorListener);
+
+
+    /**
+     *
+     * GET https://api.fitbitcom/1/user/[user-id]/activities/date/[date].json
+     *
+     * Retrieves the user's activity data by id
+     *
+     * @param cancelTag
+     * @param userId - The encoded ID of the user. Use "-" (dash) for current logged-in user
+     * @param date - The date in the format yyyy-MM-dd
+     * @param fitbitActivityListener
+     * @param errorListener
+     */
+    void getUserActivityData(Object cancelTag, String userId, String date, Response.Listener<ActivityData> fitbitActivityListener, Response.ErrorListener errorListener);
 
 
 }
